78d5b4f8283405a817a2c38525a85716b687b076
ilhamibnu <e41210178@student.polije.ac.id>
2023-07-08 07:04:14 +0700

    feat : fix chart report total suara

--- app/Http/Controllers/CalegController.php
+++ app/Http/Controllers/CalegController.php
@@ -482,6 +482,23 @@ class CalegController extends Controller
         ]);
     }
 
+    public function totalpemilih()
+    {
+        // perolehan jumlah surata suara per caleg
+
+        $totalpemilih = DB::table('tb_detail_pemilih')
+            ->join('tb_detail_relawan', 'tb_detail_pemilih.id_detail_relawan', '=', 'tb_detail_relawan.id')
+            ->join('tb_detail_tps', 'tb_detail_relawan.id_detail_tps', '=', 'tb_detail_tps.id')
+            ->join('tb_detail_desa', 'tb_detail_tps.id_detail_desa', '=', 'tb_detail_desa.id')
+            ->join('tb_desa', 'tb_detail_desa.id_desa', '=', 'tb_desa.id')
+            ->join('tb_kecamatan', 'tb_desa.id_kecamatan', '=', 'tb_kecamatan.id')
+            ->join('tb_detail_kecamatan', 'tb_detail_kecamatan.id', '=', 'tb_detail_desa.id_detail_kecamatan')
+            ->join('tb_caleg', 'tb_detail_kecamatan.id_caleg', '=', 'tb_caleg.id')
+            ->select('tb_caleg.name as caleg', DB::raw('count(tb_detail_pemilih.id) as total'))
+            ->groupBy('tb_caleg.name')
+            ->get();
+    }
+
     public function report2($id)
     {
 
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,21 +10,25 @@ class DashboardController extends Controller
     public function index()
     {
 
-        // $bypemilih = DB::table('tb_detail_pemilih')
-        // ->join('tb_caleg', 'tb_detail_pemilih.id_caleg', '=', 'tb_caleg.id')
-        // ->select('tb_caleg.name as caleg', 'tb_detail_pemilih.name as pemilih', DB::raw('count(tb_detail_pemilih.id_caleg) as total'))
-        // ->groupBy('tb_caleg.name')
-        // ->get();
+        $totalpemilih = DB::table('tb_detail_pemilih')
+            ->join('tb_detail_relawan', 'tb_detail_pemilih.id_detail_relawan', '=', 'tb_detail_relawan.id')
+            ->join('tb_detail_tps', 'tb_detail_relawan.id_detail_tps', '=', 'tb_detail_tps.id')
+            ->join('tb_detail_desa', 'tb_detail_tps.id_detail_desa', '=', 'tb_detail_desa.id')
+            ->join('tb_desa', 'tb_detail_desa.id_desa', '=', 'tb_desa.id')
+            ->join('tb_kecamatan', 'tb_desa.id_kecamatan', '=', 'tb_kecamatan.id')
+            ->join('tb_detail_kecamatan', 'tb_detail_kecamatan.id', '=', 'tb_detail_desa.id_detail_kecamatan')
+            ->join('tb_caleg', 'tb_detail_kecamatan.id_caleg', '=', 'tb_caleg.id')
+            ->select('tb_caleg.name as caleg', DB::raw('count(tb_detail_pemilih.id) as total'))
+            ->groupBy('tb_caleg.name')
+            ->get();
 
 
-        // $namacaleg = $bypemilih->pluck('caleg');
-        // $jumlahsuara = $bypemilih->pluck('total');
+        $namacaleg = $totalpemilih->pluck('caleg');
+        $jumlahsuara = $totalpemilih->pluck('total');
 
         return view('admin.pages.index', [
-            // 'namacaleg' => $namacaleg,
-            // 'jumlahsuara' => $jumlahsuara,
+            'namacaleg' => $namacaleg,
+            'jumlahsuara' => $jumlahsuara,
         ]);
-
     }
-
 }
